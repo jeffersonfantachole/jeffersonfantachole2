f32f2c5f27a63ad46b0d25c9c78b951cf548ae88
jeffersonfantachole <142330700+jeffersonfantachole@users.noreply.github.com>
2023-09-06 11:53:10 -0300

    adicionando arquivo css e js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(900, 900);
+    background("white");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+  
+    
+    
+    
+   if(mouseIsPressed) {
+     rect(mouseX, mouseY, 20, 20);
+   }
+  }
+  
